49cbb8fad9e42153ff87f97b662cfc1e02cabf03
KotaOmatsu <okota1218@icloud.com>
2022-05-28 19:06:33 +0900

    main.js修正

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue"
 import App from "./App.vue"
-import "./firebase.js"
+import "../firebase.js"
 import router from "./router"
 
 createApp(App).use(router).mount("#app")
