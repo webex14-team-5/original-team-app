bd2eac6020dd56c746944c0d9e9eeae9d6abf54d
nobuoooo <marshmellogarrix600@gmail.com>
2022-05-31 23:39:33 +0900

    会員登録とログイン機能完成

---
+++ src/views/LoginView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h2>ログイン</h2>
+    <label for="email">Email</label>
+    <input id="email" type="email" v-model="email" />
+    <br /><br />
+    <label for="password">Password</label>
+    <input id="password" type="password" v-model="password" />
+    <br /><br />
+    <button v-on:click="login">ログイン</button>
+  </div>
+</template>
+
+<script>
+import axios from "@/axios-auth"
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    }
+  },
+  methods: {
+    login() {
+      axios
+        .post(
+          "/accounts:signInWithPassword?key=AIzaSyAZMtb3rWXyqz6zo_BEmbSNEkuyBP6A_0U",
+          {
+            email: this.email,
+            password: this.password,
+            returnSecureToken: true,
+          }
+        )
+        .then((response) => {
+          console.log(response)
+        })
+    },
+  },
+}
+</script>
