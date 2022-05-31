e26ba7115fb07b68537d31ef180d33563a1ec37a
aJPcreater <nagasawatatuya@gmail.com>
2022-05-31 16:43:38 +0900

    グルメapiブランチの作成

---
+++ src/views/gourmetApi.vue
@@ -0,0 +1,11 @@
+<template></template>
+
+<script>
+fetch("https://webservice.recruit.co.jp/hotpepper/shop/v1/")
+  .then((res) => {
+    return res.json()
+  })
+  .then((data) => {
+    imageElement.src = data.message // 画像を表示する
+  })
+</script>
