41d1492530edf6b9cca0e0ea39da895f1633cc77
aJPcreater <nagasawatatuya@gmail.com>
2022-06-01 00:43:10 +0900

    CORS-error

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <gourmetApi />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue"
+import gourmetApi from "@/components/gourmetApi.vue"
 
 export default {
   name: "HomeView",
   components: {
     HelloWorld,
+    gourmetApi,
   },
 }
 </script>
