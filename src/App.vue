61b5cd8113770f7248f43959a0d6afed16d8db94
aJPcreater <nagasawatatuya@gmail.com>
2022-06-08 14:51:39 +0900

    placesAPIとgourmetAPIの比較と表示用ブランチ

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,10 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/gourmetApi">gourmetApi</router-link> | 
+    <router-link to="/gourmetApi">gourmetApi</router-link> |
     <router-link to="/login">Login</router-link> |
-    <router-link to="/register">Register</router-link>
+    <router-link to="/register">Register</router-link> |
+    <router-link to="/mergePlacesGourmet">placesGourmet</router-link>
   </nav>
   <router-view />
 </template>
---
+++ src/views/mergePlaceGourmet.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    {{ a }}
+  </div>
+  <GourmetApi></GourmetApi>
+</template>
+
+<script>
+import GourmetApi from "@/views/gourmetApi.vue"
+
+export default {
+  components: { GourmetApi },
+  data() {
+    return {
+      shopData: { results: { shop: [{ name: "" }] } },
+      shopName: "",
+      // 「 numbers 件目の店」
+      numbers: "",
+      // 条件合致する店が存在するかどうか
+    }
+  },
+  mounted: function () {
+    a = shopData.name
+  },
+}
+</script>
