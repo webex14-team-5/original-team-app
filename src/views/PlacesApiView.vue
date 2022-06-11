09846a0e187159bb2072533992d3e3213965c98c
nobuoooo <marshmellogarrix600@gmail.com>
2022-06-11 22:28:20 +0900

    途中だけど、apiapi

--- src/views/PlacesApiView.vue
+++ src/views/PlacesApiView.vue
@@ -1,34 +1,113 @@
 <template>
-  <h1>google map</h1>
-  <div ref="map" style="height: 500px; width: 800px"></div>
+  <!--検索欄-->
+  <section>
+    <!-- Access-Control-Allow-Originエラーを回避する -->
+    <header>("Access-Control-Allow-Origin: *")</header>
+    <input class="search_word" type="text" v-model="inputText" />
+    <button v-on:click="findPlace">検索</button>
+  </section>
+  <!-- 結果表示 -->
+  <section>
+    <!-- 上位10件表示 -->
+    <div v-for="(shop, numbers) in mapData.results" :key="numbers">
+      <!-- 店存在した場合で表示 -->
+      <div v-if="exit">{{ numbers + 1 }}{{ shop.name }}</div>
+    </div>
+  </section>
 </template>
 
 <script>
+import axios from "axios"
+import google from "google"
+//import fetchJsonp from "fetch-jsonp"
 export default {
   data() {
     return {
-      myLatLng: { lat: -34.397, lng: 150.644 },
+      map: new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -33.8666, lng: 151.1958 },
+        zoom: 15,
+      }),
+      query: "",
+      mapData: {},
+      exit: false,
+      placeIds: [],
     }
   },
-  mounted() {
-    const script = document.createElement("script")
-    script.src =
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyAyAIhbvR6SvjdZtEbMwRmvRk3NQmyGg0M"
-    script.async = true
-    document.head.appendChild(script)
-    let timer = setInterval(() => {
-      if (window.google) {
-        clearInterval(timer)
-        const map = new window.google.maps.Map(this.$refs.map, {
-          center: this.myLatLng,
-          zoom: 8,
+  created() {
+    // function initialize() {
+    //   // Create a map centered in Pyrmont, Sydney (Australia).
+    //   map = new google.maps.Map(document.getElementById("map"), {
+    //     center: { lat: -33.8666, lng: 151.1958 },
+    //     zoom: 15,
+    //   })
+    // }
+  },
+  methods: {
+    async findPlace() {
+      this.query = this.inputText
+      const URL =
+        "https://maps.googleapis.com/maps/api/place/textsearch/json?key=AIzaSyAyAIhbvR6SvjdZtEbMwRmvRk3NQmyGg0M&query=" +
+        this.query
+      // const res = await fetchJsonp(
+      //   "https://maps.googleapis.com/maps/api/place/textsearch/json?key=AIzaSyAyAIhbvR6SvjdZtEbMwRmvRk3NQmyGg0M&format=jsonp&query=" +
+      //     this.query
+      // )
+
+      axios
+        .get(URL)
+        .then((response) => {
+          this.mapData = response.data.results
+          this.addLocationsToGoogleMaps()
         })
-        new window.google.maps.Marker({
-          position: this.myLatLng,
-          map,
+        .catch((error) => {
+          console.log(error.message)
         })
+
+      // const res = await axios.get(URL)
+      // const data = await res.json()
+      // this.mapData = data
+      console.log(this.mapData)
+      // 条件合致する店が存在するかどうか
+      if (this.mapData != "") {
+        this.exit = true
       }
-    }, 500)
+    },
+    async getPlaceId() {
+      let request = {
+        query: this.inputText,
+      }
+      let service = new google.maps.places.PlacesService(map)
+      service.textSearch(request, this.callback_placeId)
+      for (let i = 0; i < this.placeIds.length; i++) {
+        this.getPlace(i)
+      }
+    },
+    getPlace: function (i) {
+      let request = {
+        placeId: this.placeIds[i],
+        fields: ["name", "rating", "open", "photos"],
+      }
+      let service = new google.maps.places.PlacesService(map)
+      service.textSearch(request, this.callback_search)
+    },
+    callback_search: function (results, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (let i = 1; i < results.length; i++) {
+          this.placeIds.push(results[i].place_id)
+        }
+      }
+    },
+    callback_placeId: function (results, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (let i = 1; i < results.length; i++) {
+          this.mapData.push(results[i])
+        }
+      }
+    },
   },
 }
 </script>
+<script
+  async
+  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAyAIhbvR6SvjdZtEbMwRmvRk3NQmyGg0M&callback=initMap"
+></script>
