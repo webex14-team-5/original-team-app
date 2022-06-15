3d8689da48b8bc66e8ea29c0032849c7d642be28
aJPcreater <nagasawatatuya@gmail.com>
2022-06-16 08:42:26 +0900

    SendUid Components

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,6 +2,7 @@
   <div>
     <Login @loginUid="uid = $event" />
     <Gourmet v-bind:uid="uid" />
+    <SendUid v-bind:uIdForSendUid="uid" />
     {{ uid }}
   </div>
 </template>
@@ -9,9 +10,11 @@
 <script>
 import Gourmet from "@/views/gourmetApi.vue"
 import Login from "@/components/LoginComponents.vue"
+import SendUid from "@/components/SendUid.vue"
 
 export default {
   components: {
+    SendUid,
     Gourmet,
     Login,
   },
