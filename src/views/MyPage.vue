255efed6bf5b428e7e809ed35a069b9359c3c064
aJPcreater <nagasawatatuya@gmail.com>
2022-06-22 15:18:54 +0900

    for 文での取得トライ

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -3,6 +3,7 @@
     <Login @loginUid="uid = $event" @userData="userData = $event" />
   </section>
   <section>
+    <button v-on:click="show">表示する</button>
     <div>your name: {{ this.UserName }}</div>
     <div>your fav Univ. : {{ this.myUniversities }}</div>
     <div>
@@ -16,11 +17,10 @@
 
 <script>
 import Login from "@/components/LoginComponents.vue"
-import { getDoc, getDocs, doc, collection } from "@firebase/firestore"
+import { getDoc, doc } from "@firebase/firestore"
 import { db } from "/firebase"
 
 export default {
-  props: ["uId"],
   components: {
     Login,
   },
@@ -31,23 +31,40 @@ export default {
       favShops: [],
     }
   },
-  async created() {
-    const userDataGet = await getDoc(doc(db, "users", this.uId))
-    const userData = userDataGet.data()
-    this.Username = userData.nickName
-    this.myUniversities = userData.myUniversity
-    this.favShops = userData.starPost
-    // 店の情報
-    const shopsDataGet = await getDocs(collection(db, "shops"))
-    const shopsData = shopsDataGet.data()
-    shopsData.docs.forEach((doc) => {
-      const aShopData = [
-        doc.data().name,
-        doc.data().address,
-        doc.data().genre.name,
-      ]
-      this.favShops.push(aShopData)
-    })
+  methods: {
+    async show() {
+      console.log("show start")
+      // user 情報の反映
+      const userDataGet = await getDoc(doc(db, "users", this.uid))
+      const userData = userDataGet.data()
+      this.myUniversities = userData.myUniversity
+      this.favShops = Object.values(userData.starPost)
+      this.UserName = userData.nickName
+      // 店の情報の取得
+      console.log("shops start")
+      for (let i = 0; i < this.favShops.length; i++) {
+        console.log("loop" + String(i))
+        // const document = this.favShops[i]
+        const shopsDataGet = await getDoc(db, "shops", "J000760558")
+        const shopsData = shopsDataGet.data()
+        console.log(shopsDataGet)
+        const aShopData = [
+          shopsData.name,
+          shopsData.address,
+          shopsData.genre.name,
+        ]
+        // shopsData.docs.forEach((doc) => {
+        //   const aShopData = [
+        //     doc.data().name,
+        //     doc.data().address,
+        //     doc.data().genre.name,
+        //   ]
+        //   this.favShops.push(aShopData)
+        // })
+        this.favShops.push(aShopData)
+      }
+      console.log(this.favShops)
+    },
   },
 }
 </script>
